2f6970b2f56fd60665f37ac7a98ca86670e9fa3b
kim-seung-joo <89231023+kim-seung-joo@users.noreply.github.com>
2025-11-23 02:38:07 +0900

    Delete 데이터와 코딩 directory

--- 데이터와 코딩/index.js
+++
@@ -1,8 +0,0 @@
-var btn = document.getElementById(".btn");
-function login(){
-    alert("로그인을 하지않으면 사용불가합니다.");
-    alert("로그인페이지로 이동합니다.")
-    setTimeout(function() {
-        window.location.href = "login.html";
-      });
-}
